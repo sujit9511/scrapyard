bfc50a18e420bf140008ffc39319d8c28c469c18
sujit9511 <sujitgaikwad9511@gmail.com>
2022-09-20 20:25:27 +0530

    minor css change with navbar

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,19 +1,17 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-
+import CSSnavigation from "./CSSnavigation.css";
 function Navigation() {
   return (
     <>
       <Navbar bg="primary" variant="dark" fixed="top">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
+        <Navbar.Brand href="#home">Scrapyard</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
       </Navbar>
     </>
   );
